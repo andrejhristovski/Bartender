74000e6568edacd4a38801f174a6e9c398fc0913
Andrej Hristovski <andrejhristovski@outlook.com>
2026-09-06 11:46:23 +0200

    Fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -164,7 +164,7 @@ export default function Home() {
                       <circle cx="12" cy="12" r="4" />
                       <path d="M17.5 6.5h.01" />
                     </svg>
-                    @{site.instagram}
+                    Instagram
                   </a>
                 )}
                 {site.cv && (
